32203cc030a5a7f23de3ea5caa439a6980889c81
isalvama <inesalvans@gmail.com>
2026-04-27 19:22:42 +0200

    refactor: move away MenuBuilder class to a new file and create nested step classes for progressive building to work properly, add necessary getters and setters in Menu class, change name of Desert class to Dessert

--- src/main/java/builder/menu/Menu.java
+++ src/main/java/builder/menu/Menu.java
@@ -1,53 +1,51 @@
 package builder.menu;
 
-import builder.plates.Desert;
+import builder.plates.Dessert;
 import builder.plates.EntrantFoodPlate;
 import builder.plates.MainFoodPlate;
 
-import java.util.List;
-
-/**
- * Implementar una restricció per la qual, si es tria postres, no es podrà afegir cafè, i viceversa.
- * Aquesta regla s’ha de garantir gràcies al disseny d’interfícies.
- * Permetre configurar les propietats dels plats (vegà, apta per celíacs, suplement) amb mètodes fluents i expressius,
- * evitant paràmetres booleanos.
- * Implementa tests unitaris on es mostri la creació diversos menús:
- */
-
 public class Menu {
-    // ordre correcte i obligatori: no es podrà afegir postres abans d’afegir el plat principal
     private EntrantFoodPlate entrant;
     private MainFoodPlate mainCourse;
-    private List<Desert> postres;
-    // Les postres són opcionals a tots els menus i poden ser postres o cafè.
+    private Dessert dessert;
     private String drink;
     private String coffe;
 
-    private Menu(){}
-    public static class MenuBuilder{
-        private Menu menuManagedInstance = new Menu();
+    Menu(){}
 
-        public MenuBuilder withEntrant (String name){
-            this.menuManagedInstance.entrant = new EntrantFoodPlate.Builder().withEntrant(name).build();
-            return this;
-        }
+    public void setEntrant(EntrantFoodPlate entrant) {
+        this.entrant = entrant;
+    }
 
-        public MenuBuilder isGlutenFree() {
+    public void setDessert(Dessert dessert) {
+        this.dessert = dessert;
+    }
 
-            this.menuManagedInstance != null ? this.menuManagedInstance.entrant. =
-            }
-            return this;
-        }
+    public void setDrink(String drink) {
+        this.drink = drink;
+    }
 
-        public Menu withMainCourse(){
+    public void setCoffee(String coffeName) {
+        this.coffe = coffeName;
+    }
 
-        }
+    public Dessert getDessert() {
+        return dessert;
+    }
 
-        public Menu build() {
-            return menuManagedInstance;
-        }
+    public String getCoffe() {
+        return coffe;
+    }
 
+    public EntrantFoodPlate getEntrant() {
+        return entrant;
+    }
 
+    public MainFoodPlate getMainCourse() {
+        return mainCourse;
+    }
 
+    public void setMainCourse(MainFoodPlate mainCourse) {
+        this.mainCourse = mainCourse;
     }
 }
